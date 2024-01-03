122b23c06c8c095abfe1066c92fcbbb1bcc9d3c3
Hardik Vala <hardikvala24@gmail.com>
2024-01-02 21:32:11 -0500

    Filter and display goals.

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
+import browser_cookie3
+import requests
 import streamlit as st
 
 def main():
-  # Set up the title of the app
-  st.title("Simple Streamlit App")
+  cj = browser_cookie3.chrome(domain_name="workflowy.com")
 
-  # Input from the user
-  user_input = st.text_input("Enter some text")
+  r = requests.get(
+      "https://workflowy.com/get_tree_data",
+      cookies=cj,
+      headers={"Accept": "application/json"},
+  )
 
-  # Display the input
-  if user_input:
-    st.write("You entered:", user_input)
+  workflowy_tree_data = r.json()
+
+  goals = [i["nm"] for i in workflowy_tree_data["items"] if "#Goal" in i["nm"]]
+
+  st.title("Hardik's PTM")
+  st.write(goals)
 
 if __name__ == "__main__":
   main()
