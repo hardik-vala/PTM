ac11f5d0cbe06cb59248b121246f2a15d4c3f2d0
Hardik Vala <hardikvala24@gmail.com>
2024-01-05 21:44:32 -0500

    Display ancestor tasks.

--- src/app.py
+++ src/app.py
@@ -38,6 +38,29 @@ class WorkflowyService:
         return r.json()
 
 
+class TaskList:
+    def __init__(self, tasks: List[Task]):
+        self.tasks = tasks
+        self.task_map = None
+
+    def getTaskMap(self) -> Dict[str, Task]:
+        if not self.task_map:
+            self.task_map = {t.id: t for t in self.tasks}
+
+        return self.task_map
+
+    def getAncestors(self, task_id):
+        task_map = self.getTaskMap()
+
+        ancestors = []
+        ancestor_id = task_map[task_id].parent_id
+        while ancestor_id:
+            ancestors.append(ancestor_id)
+            ancestor_id = task_map[ancestor_id].parent_id
+
+        return ancestors[::-1]
+
+
 class TaskStore:
     def __init__(self, workflowy_service: WorkflowyService):
         self.workflowy_service = workflowy_service
@@ -61,13 +84,19 @@ class TaskStore:
     def _strip_due_date(self, workflowy_item_nm: str) -> str:
         return re.sub(r", Due <time .*?</time>", "", workflowy_item_nm)
 
-    def _strip_tags(self, workflowy_item_nm: str) -> str:
+    def _strip_html_tags(self, workflowy_item_nm: str) -> str:
+        clean = re.compile("<.*?>")
+        return re.sub(clean, "", workflowy_item_nm)
+
+    def _strip_hashtags(self, workflowy_item_nm: str) -> str:
         return re.sub(r"#(\w+)", "", workflowy_item_nm)
 
     def _extract_task_name(self, workflowy_item: Dict) -> str:
-        return self._strip_tags(self._strip_due_date(workflowy_item["nm"]))
+        return self._strip_html_tags(
+            self._strip_hashtags(self._strip_due_date(workflowy_item["nm"]))
+        )
 
-    def fetch_tasks(self) -> List[Task]:
+    def fetch_tasks(self) -> TaskList:
         tree_data = self.workflowy_service.fetch_tree_data()
 
         tasks = []
@@ -83,7 +112,7 @@ class TaskStore:
             )
             tasks.append(task)
 
-        return tasks
+        return TaskList(tasks)
 
 
 def calculate_next_sunday(day: datetime) -> datetime:
@@ -97,7 +126,11 @@ def calculate_next_sunday(day: datetime) -> datetime:
 def main():
     workflow_service = WorkflowyService()
     task_store = TaskStore(workflow_service)
-    tasks = task_store.fetch_tasks()
+    task_list = task_store.fetch_tasks()
+    task_map = task_list.getTaskMap()
+
+    def get_ancestor_str(task_id):
+        return " > ".join([task_map[id].name for id in task_list.getAncestors(task_id)])
 
     st.title("Hardik's PTM")
 
@@ -107,17 +140,31 @@ def main():
     next_sunday = calculate_next_sunday(datetime.today())
 
     rows = []
-    for task in tasks:
+    for task in task_list.tasks:
         if task.is_goal and not task.is_completed:
             if filter_this_week:
                 if task.due_date and task.due_date <= next_sunday:
-                    rows.append([task.name, task.due_date_str, ", ".join(task.tags)])
+                    rows.append(
+                        [
+                            task.name,
+                            get_ancestor_str(task.id),
+                            task.due_date_str,
+                            ", ".join(task.tags),
+                        ]
+                    )
             else:
-                rows.append([task.name, task.due_date_str, ", ".join(task.tags)])
+                rows.append(
+                    [
+                        task.name,
+                        get_ancestor_str(task.id),
+                        task.due_date_str,
+                        ", ".join(task.tags),
+                    ]
+                )
 
     rows.sort(key=lambda r: r[1] or datetime(9999, 12, 31).strftime("%Y.%m.%d"))
 
-    df = pd.DataFrame(rows, columns=["Task", "Due Date", "Tags"])
+    df = pd.DataFrame(rows, columns=["Task", "Ancestors", "Due Date", "Tags"])
 
     st.table(df)
 
